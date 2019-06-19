d29e980ff8f6af80d75e7da3a807462cefd03ba2
Leandro Fernandes <leanoficialcontato@gmail.com>
2019-06-19 10:56:24 -0300

    adicionando preço na sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -22,10 +24,13 @@ public class Sessao {
 	@ManyToOne
 	private	Filme filme;
 	
+	private BigDecimal preco;
+	
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.setHorario(horario);
 					this.setFilme(filme);
 					this.setSala(sala);
+					this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
 	public Integer getId() {
@@ -65,5 +70,13 @@ public class Sessao {
 	public Sessao () {
 		
 	}
+	
+	public BigDecimal getPreco() {
+    	return preco.setScale(2, RoundingMode.HALF_UP);
+    }
+    
+    public void setPreco(BigDecimal preco) {
+    	this.preco = preco;
+    }
 	
 }
