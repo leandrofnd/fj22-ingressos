5c83a43701eca9f93c275c888f8337b6020a6a38
Leandro Fernandes <leanoficialcontato@gmail.com>
2019-06-20 15:30:55 -0300

    adicionando tela de detalhes da compra

--- src/main/java/br/com/caelum/ingresso/controller/CompraController.java
+++ src/main/java/br/com/caelum/ingresso/controller/CompraController.java
@@ -1,6 +1,8 @@
 package br.com.caelum.ingresso.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,6 +11,7 @@ import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Carrinho;
 import br.com.caelum.ingresso.model.form.CarrinhoForm;
 
+@Controller
 public class CompraController {
 	
 	@Autowired
@@ -26,6 +29,14 @@ public class CompraController {
 	
 		carrinhoForm.toIngressos(sessaoDao, lugarDao).forEach(carrinho::add);
 		
+		return modelAndView;
+	}
+	
+	@GetMapping("/compra")
+	public ModelAndView checkout(){
+		
+		ModelAndView modelAndView = new ModelAndView("compra/pagamento");
+		modelAndView.addObject("carrinho", carrinho);
 		return modelAndView;
 	}
 }
